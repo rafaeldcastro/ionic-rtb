06b7b2839404ce754b7804ace2b80da9779248bf
Rafael <rafaelcastro@midiaseducativas.com.br>
2020-06-04 13:23:58 -0300

    home with infinite scroll and virtual scroll. Proceding to make the components

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,6 +8,67 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  users = [];
+  page = 0;
+  maximumPages = 3;
 
+  constructor(
+    private http: HttpClient
+  ) {
+    // this.loadUsers();
+    this.loadUsers_VirtualScroll();
+  }
+
+  loadUsers(event?){
+    this.http.get(`https://randomuser.me/api/?results=20&page=${this.page}`)
+      .subscribe(res => {
+        console.log(res)
+        this.users = this.users.concat(res['results']);
+
+        if(event){
+          event.target.complete();
+        }
+      })
+  }
+
+  loadMore(event){
+    this.page++;
+    this.loadUsers(event);
+
+    if(this.page === this.maximumPages){
+      event.target.disabled = true;
+    }
+  }
+
+  loadUsers_VirtualScroll(){
+    this.http.get(`https://randomuser.me/api/?results=100&page`)
+      .subscribe(res => {
+        
+        this.users = res['results'].sort((a,b) => {
+          if(a.name.last < b.name.last){
+            return -1;
+          }
+          if(a.name.last > b.name.last){
+            return 1;
+          }
+          return 0;
+        })
+        console.log(this.users)
+      })
+  }
+
+  separateLetter(record, recordIndex, records){
+
+    if(recordIndex == 0){
+      return record.name.last[0].toUpperCase();
+    }
+
+    let first_prev = records[recordIndex - 1].name.last[0];
+    let first_current  = record.name.last[0];
+
+    if(first_prev != first_current){
+      return first_current.toUpperCase();
+    }
+    return null;
+  }
 }
